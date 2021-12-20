a04accb704373d86fe6ffc8a78abd716be36ab8b
Mikael Petersen <info@mikaelpetersen.se>
2021-12-20 14:33:00 +0100

    ex 10-7

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Pressable, View, StyleSheet } from "react-native";
+import { Pressable, ScrollView, View, StyleSheet } from "react-native";
 import { useHistory } from "react-router-native";
 import theme from "../theme";
 import Constants from "expo-constants";
@@ -8,36 +8,55 @@ import Text from "./Text";
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.purple,
-    height: 100,
-    alignItems: "stretch",
-    justifyContent: "space-evenly",
+    height: 90,
   },
   pressable: {
+    backgroundColor: "rgba(255,255,255,.2)",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    marginLeft: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  tabText: {
+    color: "#ffffff"
   }
 });
 
-const AppBar = () => {
+const TabBarItem = ({ to, label }) => {
   const history = useHistory();
 
   return (
+    <Pressable
+      style={styles.pressable}
+      onPress={() => history.push(to)}
+    >
+      <Text fontSize="subheading" fontWeight="bold" style={styles.tabText}>
+        {label}
+      </Text>
+    </Pressable>
+  );
+};
+
+const AppBar = () => {
+
+  return (
     <View style={styles.container}>
-      <Pressable style={styles.pressable} onPress={() => history.push("/")}>
-        <Text fontSize="subheading" fontWeight="bold" style={{ color: "#fff" }}>
-          Repositories
-        </Text>
-      </Pressable>
-      <Pressable style={styles.pressable} onPress={() => history.push("/sign-in")}>
-        <Text fontSize="subheading" fontWeight="bold" style={{ color: "#fff" }}>
-          Sign in
-        </Text>
-      </Pressable>
+      <ScrollView horizontal>
+        <TabBarItem
+          to="/"
+          label="Repositories"
+        />
+        <TabBarItem
+          to="/sign-in"
+          label="Sign in"
+        />
+
+      </ScrollView>
     </View>
   );
 };
